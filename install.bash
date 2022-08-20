c8cc1d315895080e4729f2ec7bcea68e3d77e201
Rachid oudouch <rashidoudouch@gmail.com>
2022-08-20 16:47:36 +0100

    add bclean

--- install.bash
+++ install.bash
@@ -15,10 +15,10 @@ startInstall() {
         echo "alias storage=\"bash $path/check_space.bash\"" >> ~/.zshrc
         echo "alias bleach=\"bash $path/help.bash\"" >> ~/.zshrc
         echo "alias unBleach=\"bash $path/unbleach.bash\"" >> ~/.zshrc
+        echo "alias bclean=\"bash $bleachPath\"" >> ~/.zshrc
         echo -e "####10108bleach10108####" >> ~/.zshrc
         printf "\033[32mInstalled ✅ \033[0m\n"
         printf "Please reopen terminal\n"
-
     else
         printf "\033[32mAlready installed!✅ \033[0m\n\n"
     fi
