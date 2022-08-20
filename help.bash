2a8efe4bf17f64d2de1420eda82f1710115e8d4a
Rachid oudouch <rashidoudouch@gmail.com>
2022-08-20 16:07:38 +0100

    update info

--- help.bash
+++ help.bash
@@ -9,7 +9,7 @@ echo -e "\033[33m
                 Created by \033[0;32mRachid Oudouch
 \033[0m
 "
-printf "\n\033[0;33mBleach is Running always when zsh is running, you don't need to do anything.\033[0m"
+printf "\n\033[0;33mBleach will always run when you open the terminal, you don't need to do anything.\033[0m"
 printf "\n\n\033[0;34m----- Command line you can run -----\033[0m\n\n"
 printf "|- To see your free space: \033[4;36mstorage\n\n\033[0m"
 printf "|- To Uninstall Bleach: \033[4;36munBleach\n\n\033[0m"
