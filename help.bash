ad2841bc2da9ebbd257040332568e4ae3293bf85
Rachid Oudouch <36020015+Ra-Wo@users.noreply.github.com>
2022-08-21 16:53:21 +0100

    update command of uninstall

--- help.bash
+++ help.bash
@@ -13,7 +13,7 @@ printf "\n\033[0;33mBleach will always run when you open the terminal, you don't
 printf "\n\n\033[0;34m----- Command line you can run -----\033[0m\n\n"
 printf "|- To clean your computer: \033[4;36mbclean\n\n\033[0m"
 printf "|- To see your free space: \033[4;36mstorage\n\n\033[0m"
-printf "|- To Uninstall Bleach: \033[4;36munBleach\n\n\033[0m"
+printf "|- To Uninstall Bleach: \033[4;36munbleach\n\n\033[0m"
 printf "|- for help: \033[4;36mbleach\n\n\033[0m"
 
 echo -e "\n\n*************************************************************\n"
