6a46ae8d40f97d4c897f15b7a0da23f740d501c8
Rachid oudouch <rashidoudouch@gmail.com>
2022-08-22 14:23:28 +0100

    add dark-mode

--- help.bash
+++ help.bash
@@ -1,19 +1,78 @@
 #!/bin/bash
 #Author Rachid Oudouch
 #42login : roudouch
-echo -e "\n\n*************************************************************\n"
-echo -e "\033[33m
-                   ╔╗ ┬  ┌─┐┌─┐┌─┐┬ ┬ 
-                   ╠╩╗│  ├┤ ├─┤│  ├─┤
-                   ╚═╝┴─┘└─┘┴ ┴└─┘┴ ┴
-                Created by \033[0;32mRachid Oudouch
-\033[0m
-"
-printf "\n\033[0;33mBleach will always run when you open the terminal, you don't need to do anything.\033[0m"
-printf "\n\n\033[0;34m----- Command line you can run -----\033[0m\n\n"
-printf "|- To clean your computer: \033[4;36mbclean\n\n\033[0m"
-printf "|- To see your free space: \033[4;36mstorage\n\n\033[0m"
-printf "|- To Uninstall Bleach: \033[4;36munbleach\n\n\033[0m"
-printf "|- for help: \033[4;36mbleach\n\n\033[0m"
 
-echo -e "\n\n*************************************************************\n"
+function deleteService() {
+    shouldRemove='0'
+    while read -r line
+    do
+        if [[ ! $line =~ $1 ]]
+        then
+            if [ $shouldRemove == '0' ]
+            then
+                echo "$line"
+            fi
+        else
+            if [ $shouldRemove == '0' ]
+            then
+                shouldRemove='1'
+            elif [ $shouldRemove == '1' ]
+            then
+                shouldRemove='0'
+            fi
+        fi
+    done < ~/.zshrc > O1234567899876543210Darkmode
+
+    $(cat O1234567899876543210Darkmode > ~/.zshrc)
+    $(rm -rf O1234567899876543210Darkmode)
+}
+
+
+if [ "$1" == "active" ]
+then
+    if [ "$2" == "dark-mode" ]
+    then
+        alreadyInstall=$(grep "####10108bleach-Dark-mode10108####" < ~/.zshrc)
+        if [  ${#alreadyInstall} == 0 ]
+        then
+            $(osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true')
+            echo -e "####10108bleach-Dark-mode10108####" >> ~/.zshrc
+            echo -e "osascript -e 'tell app \"System Events\" to tell appearance preferences to set dark mode to true'" >> ~/.zshrc
+            echo -e "####10108bleach-Dark-mode10108####" >> ~/.zshrc
+            printf "\n\033[32mDark-mode activated ✅ \033[0m\n\n"
+        else
+            printf "\n\033[0;31mDark-mode is already activated!\033[0m\n\n"
+        fi
+    fi
+elif [ "$1" == "deactivate" ]
+then
+    if [ "$2" == "dark-mode" ]
+    then
+        alreadyInstall=$(grep "####10108bleach-Dark-mode10108####" < ~/.zshrc)
+        if [  ${#alreadyInstall} == 0 ]
+        then
+            printf "\n\033[0;31mDark-mode is not activated!\033[0m\n\n"
+        else
+            deleteService "####10108bleach-Dark-mode10108####"
+            $(osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false')
+            printf "\n\033[32mDark-mode deactivated ✅ \033[0m\n\n"
+        fi
+    fi
+else
+    echo -e "\033[33m
+                    ╔╗ ┬  ┌─┐┌─┐┌─┐┬ ┬ 
+                    ╠╩╗│  ├┤ ├─┤│  ├─┤
+                    ╚═╝┴─┘└─┘┴ ┴└─┘┴ ┴
+                    Created by \033[0;32mRachid Oudouch
+    \033[0m
+    "
+    printf "\n\033[0;33mBleach will always run when you open the terminal, you don't need to do anything.\033[0m"
+    printf "\n\n\033[0;34m----- Command line you can run -----\033[0m\n\n"
+    printf "|- To clean your computer: \033[4;36mbclean\n\n\033[0m"
+    printf "|- To see your free space: \033[4;36mstorage\n\n\033[0m"
+    printf "|- To Uninstall Bleach: \033[4;36munbleach\n\n\033[0m"
+    printf "|- To active dark-mode: \033[4;36mbleach active dark-mode\n\n\033[0m"
+    printf "|- To deactivate dark-mode: \033[4;36mbleach deactivate dark-mode\n\n\033[0m"
+    printf "|- for help: \033[4;36mbleach\n\n\033[0m"
+
+fi
