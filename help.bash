a71970231b1c74ac22f8fa5c8d01db06f8cc59b3
Rachid oudouch <rashidoudouch@gmail.com>
2022-08-22 14:27:23 +0100

    edit help command (bleach)

--- help.bash
+++ help.bash
@@ -64,10 +64,9 @@ else
                     ╠╩╗│  ├┤ ├─┤│  ├─┤
                     ╚═╝┴─┘└─┘┴ ┴└─┘┴ ┴
                     Created by \033[0;32mRachid Oudouch
-    \033[0m
-    "
+    \033[0m"
     printf "\n\033[0;33mBleach will always run when you open the terminal, you don't need to do anything.\033[0m"
-    printf "\n\n\033[0;34m----- Command line you can run -----\033[0m\n\n"
+    printf "\n\n\033[0;35m------- Command line you can run -------\033[0m\n\n"
     printf "|- To clean your computer: \033[4;36mbclean\n\n\033[0m"
     printf "|- To see your free space: \033[4;36mstorage\n\n\033[0m"
     printf "|- To Uninstall Bleach: \033[4;36munbleach\n\n\033[0m"
